abf51d61ea5de63a8317c8db82875bf085903b04
Javokhir Shomuratov <javohirshomuradov@gmail.com>
2021-03-24 12:48:51 +0500

    task 1 added comments

--- lab6-semester2/1.cpp
+++ lab6-semester2/1.cpp
@@ -1,31 +1,41 @@
 #include<iostream>
 using namespace std;
 
+//defining Rectangle class
 class Rectangle{
     private:
+        //private members
         double length, breadth;
     public:
+        //public members
+        //default constructor
         Rectangle();
+        //parametric constructor
         Rectangle(double length, double breadth){
             this->length = length;
             this->breadth = breadth;
         }
+        //get area function
         double getArea(){
             return this->length * this->breadth;
         }
+        //setters
         void setLength(double length){
             this->length = length;
         }
         void setBreadth(double breadth){
             this->breadth = breadth;
         }
+        //overloading (+) operator
         Rectangle operator +(Rectangle addedRectangle){
+            //returning new Rectangle object
             return Rectangle(this->length + addedRectangle.length, this->breadth + addedRectangle.breadth);
         }
 
 };
-
+//main class
 int main(){
+    //initializing rectangles
     Rectangle rect1(7,8);
     cout<<"Area of rectangle 1: "<<rect1.getArea()<<endl;
 
@@ -35,6 +45,7 @@ int main(){
 
     cout<<"Area of rectangle 2: "<<rect2.getArea()<<endl;;
     
+    //using our overloaded function to add two rectangles and get new one;
     Rectangle rect3 = rect1 + rect2;
     cout<<"Area of rectangle 3 which is addition of two rectangles: "<<rect3.getArea()<<endl;;
 

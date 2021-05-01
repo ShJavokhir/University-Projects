e293da78cdfce9fc465b0696fe09b6c86b206263
Javokhir Shomuratov <javohirshomuradov@gmail.com>
2021-05-01 12:10:06 +0500

    done

--- lab11-semester2/1.cpp
+++ lab11-semester2/1.cpp
@@ -6,25 +6,30 @@
     Github: https://github.com/shjavokhir
 */
 
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 //defining Person class
-class Person{
-    private:
-        string name, telNumber;
-    public:
-        void setData(string name, string telNumber){
-            this->name = name;
-            this->telNumber = telNumber;
-        }
-        void printData(){
-            cout<<"Name: "<<name<<" TelNumber: "<<telNumber<<endl;
-        }
+class Person
+{
+private:
+    string name, telNumber;
+
+public:
+    void setData(string name, string telNumber)
+    {
+        this->name = name;
+        this->telNumber = telNumber;
+    }
+    void printData()
+    {
+        cout << "Name: " << name << " TelNumber: " << telNumber << endl;
+    }
 };
 
 //function to get input from user then write to the file
-void writeToFile(){
+void writeToFile()
+{
     ofstream ofs;
     Person person;
     char usersChoice;
@@ -32,27 +37,31 @@ void writeToFile(){
     //opening binary file (binary file will be created if it is not exist)
     ofs.open("data", ios::binary);
 
-    while(true){
-        cout<<"Enter y or Y to write to the file: ";
-        cin>>usersChoice;
-        if(usersChoice == 'y' || usersChoice == 'Y' ){
-            cout<<"Name: ";
-            cin>>name;
-            cout<<"Tel Number: ";
-            cin>>telNumber;
+    while (true)
+    {
+        cout << "Enter y or Y to write to the file: ";
+        cin >> usersChoice;
+        if (usersChoice == 'y' || usersChoice == 'Y')
+        {
+            cout << "Name: ";
+            cin >> name;
+            cout << "Tel Number: ";
+            cin >> telNumber;
             person.setData(name, telNumber);
             //writing to the binary file
-            ofs.write((char*)&person, sizeof(Person));
-        }else{
+            ofs.write((char *)&person, sizeof(Person));
+        }
+        else
+        {
             //the case when user inputs characters other than y and Y
             break;
         }
-
     }
     ofs.close();
 }
 
-void readFromFile(){
+void readFromFile()
+{
     //function to read data from binary file
     ifstream ifs;
     Person person;
@@ -60,16 +69,18 @@ void readFromFile(){
     ifs.open("data", ios::binary);
     //clearing screen
     system("cls");
-    cout<<"Below is the data inside the file: "<<endl;
+    cout << "Below is the data inside the file: " << endl;
     //reading all the content of the binary file
-    while(ifs.read((char*)&person, sizeof(Person))){
+    while (ifs.read((char *)&person, sizeof(Person)))
+    {
         person.printData();
     }
     ifs.close();
 }
 
 //clean main function
-int main(){
+int main()
+{
     writeToFile();
     readFromFile();
     return 0;
--- lab11-semester2/2.cpp
+++ lab11-semester2/2.cpp
@@ -2,197 +2,210 @@
     This code is written by Javokhir Shomuratov
     U2010257
     Started at; 21:19 22-april 2021
-    Finished at: not finished yet
+    Finished at: 12:09 23-april 2021
     Telegram: @shjavohir
     Github: https://github.com/shjavokhir
 */
 
-
-
-/*
-
-DISCLAIMER
-
-
-I AM VERY TIRED OF THIS harmful (bad) PROGRAM, IT DOES NOT WORK PROPERLY, CPP is very .....
-
-DONT COPY THIS TASK YET
-
-I spent about 2.30 hours for this ***. But there are still bugs with addRecord function.....
-
-I hope i will fix them tomorrow
-
-
-*/
-
-
-
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-ifstream ifs;
-ofstream ofs;
-ofstream tempOfs;
-
 //defining Person class
-class Person{
-    private:
-        string name, telNumber;
-    public:
-        void setData(string name, string telNumber){
-            this->name = name;
-            this->telNumber = telNumber;
-        }
-        void printData(){
-            cout<<"Name: "<<name<<" TelNumber: "<<telNumber<<endl;
-        }
-        string getName(){
-            return name;
-        }
-        string getTelNumber(){
-            return telNumber;
-        }
+class Person
+{
+private:
+    string name, telNumber;
 
+public:
+    void setData(string name, string telNumber)
+    {
+        this->name = name;
+        this->telNumber = telNumber;
+    }
+    void printData()
+    {
+        cout << "Name: " << name << " TelNumber: " << telNumber << endl;
+    }
+    string getName()
+    {
+        return name;
+    }
+    string getTelNumber()
+    {
+        return telNumber;
+    }
 };
 Person person;
 
-void openFiles(){
+void getTelNumberByName()
+{
+    //object of ofstream class
+    ifstream ifs;
+    //opening file for reading in binary mode
     ifs.open("data", ios::binary);
-    ofs.open("data", ios::binary | fstream::in | fstream::out);
-    tempOfs.open("temp", ios::binary);
-}
-void closeFiles(){
-    ifs.close();
-    ofs.close();
-    tempOfs.close();
-}
-void getTelNumberByName(){
-    ifs.clear();
-    ifs.seekg(0, ios::beg);
-    system("clear");
+    //clearing screen
+    system("cls");
     string name;
-    cout<<"Enter name to get corresponding tel number: ";
-    cin>>name;
-    while(ifs.read((char *)&person, sizeof(Person))){
-        if(person.getName() == name){
-            cout<<person.getTelNumber()<<" is the tel number you want"<<endl;
+    cout << "Enter name to get corresponding tel number: ";
+    cin >> name;
+    //looping through contents of the file
+    while (ifs.read((char *)&person, sizeof(Person)))
+    {
+        if (person.getName() == name)
+        {
+            cout << person.getTelNumber() << " is the tel number you want" << endl;
+            ifs.close();
             return;
         }
     }
-    cout<<"Name not found;"<<endl;
+    //cloing file
+    ifs.close();
+    cout << "Name not found;" << endl;
 }
 
-void getNameByTelNumber(){
-    ifs.clear();
-    ifs.seekg(0, ios::beg);
-    system("clear");
+void getNameByTelNumber()
+{
+    ifstream ifs;
+    ifs.open("data", ios::binary);
+    system("cls");
     string telNumber;
-    cout<<"Enter tel number to get corresponding name: ";
-    cin>>telNumber;
-    while(ifs.read((char *)&person, sizeof(Person))){
-        if(person.getTelNumber() == telNumber){
-            cout<<person.getName()<<" is the name you want"<<endl;
+    cout << "Enter tel number to get corresponding name: ";
+    cin >> telNumber;
+    while (ifs.read((char *)&person, sizeof(Person)))
+    {
+        if (person.getTelNumber() == telNumber)
+        {
+            cout << person.getName() << " is the name you want" << endl;
+            ifs.close();
             return;
         }
     }
-    cout<<"Tel number not found"<<endl;
+    ifs.close();
+    cout << "Tel number not found" << endl;
 }
 
-void deleteRecordByTelNumber(){
-    ifs.clear();
-    ifs.seekg(0, ios::beg);
+void deleteRecordByTelNumber()
+{
+
+    ifstream ifs;
+    ofstream ofs;
+    ofstream tempOfs;
+    system("cls");
+    tempOfs.open("temp", ios::binary);
+    ofs.open("data", ios::binary | ios::app);
+    ifs.open("data", ios::binary);
+
     string telNumber;
     bool isFound = false;
-    cout<<"Enter tel number of the record you want to delete: ";
-    cin>>telNumber;
+    cout << "Enter tel number of the record you want to delete: ";
+    cin >> telNumber;
 
-    while(ifs.read((char *)&person, sizeof(Person))){
-        if(person.getTelNumber() != telNumber){
+    while (ifs.read((char *)&person, sizeof(Person)))
+    {
+        if (person.getTelNumber() != telNumber)
+        {
             tempOfs.write((char *)&person, sizeof(Person));
-        }else{
+        }
+        else
+        {
             isFound = true;
         }
     }
 
-    
+    //deleting file named data
     remove("data");
+    //renaming file named temp into data
     rename("temp", "data");
-    if(isFound){
-        cout<<"Record with tel number of "<<telNumber<<" is deleted"<<endl;
+    if (isFound)
+    {
+        cout << "Record with tel number of " << telNumber << " is deleted" << endl;
     }
-    closeFiles();
-    openFiles();
-
+    ifs.close();
+    ofs.close();
+    tempOfs.close();
 }
 
-void addRecord(){
+void addRecord()
+{
+    ofstream ofs;
+    ofs.open("data", ios::binary | ios::in | ios::out);
+    system("cls");
     int positionToAdd;
     string name, telNumber;
-    cout<<"Enter the position to add new record: ";
-    cin>>positionToAdd;
-    cout<<"Name: ";
-    cin>>name;
-    cout<<"Tel number: ";
-    cin>>telNumber;
+    cout << "Enter the position to add new record: ";
+    cin >> positionToAdd;
+    cout << "Name: ";
+    cin >> name;
+    cout << "Tel number: ";
+    cin >> telNumber;
     person.setData(name, telNumber);
-    //ofs.seekp(0, ios::end);
-    //int totalBytes = ifs.tellg();
-    //int totalRecords = totalBytes / (sizeof(Person)*1.0);
+    //getting position to replace file content
     int realPositionToAdd = sizeof(Person) * (positionToAdd - 1);
+    //moving bytes from beginning
     ofs.seekp(realPositionToAdd);
+    //writing to the file
     ofs.write((char *)&person, sizeof(Person));
+    ofs.close();
 }
 
-void showFileContents(){
-    ifs.clear();
-    ifs.seekg(0, ios::beg);
+void showFileContents()
+{
+    ifstream ifs;
+    ifs.open("data", ios::binary);
+
     system("cls");
-    cout<<"Below is the data inside the file: "<<endl;
+    cout << "Below is the data inside the file: " << endl;
     //reading all the content of the binary file
-    while(ifs.read((char*)&person, sizeof(Person))){
+    while (ifs.read((char *)&person, sizeof(Person)))
+    {
         person.printData();
     }
+    ifs.close();
 }
 
-void showMenu(){
+void showMenu()
+{
     int usersChoice;
-    while(true){
-        cout<<"<=== MENU ===>"<<endl;
-        cout<<"1) Get tel number by name"<<endl;
-        cout<<"2) Get name by tel number"<<endl;
-        cout<<"3) Delete record by tel number"<<endl;
-        cout<<"4) Add record to the specific location"<<endl;
-        cout<<"5) Print file contents"<<endl;
-        cout<<"0) Exit"<<endl<<endl;
-        cout<<"Your choice: ";
-        cin>>usersChoice;
-        switch(usersChoice){
-            case 1:
-                getTelNumberByName();
-                break;
-            case 2:
-                getNameByTelNumber();
-                break;
-            case 3:
-                deleteRecordByTelNumber();
-                break;
-            case 4:
-                addRecord();
-                break;
-            case 5:
-                showFileContents();
-            default:
-                break;
+    while (true)
+    {
+        cout << "<=== MENU ===>" << endl;
+        cout << "1) Get tel number by name" << endl;
+        cout << "2) Get name by tel number" << endl;
+        cout << "3) Delete record by tel number" << endl;
+        cout << "4) Add record to the specific location" << endl;
+        cout << "5) Print file contents" << endl;
+        cout << "6) Exit" << endl
+             << endl;
+        cout << "Your choice: ";
+        cin >> usersChoice;
+        switch (usersChoice)
+        {
+        case 1:
+            getTelNumberByName();
+            break;
+        case 2:
+            getNameByTelNumber();
+            break;
+        case 3:
+            deleteRecordByTelNumber();
+            break;
+        case 4:
+            addRecord();
+            break;
+        case 5:
+            showFileContents();
+            break;
+        default:
+            exit(0);
         }
+        system("pause");
     }
 }
 
-
 //clean main function
-int main(){
-    openFiles();
+int main()
+{
     showMenu();
-    closeFiles();
     return 0;
 }
